955b28742740724d7bce3e70976a1a00d8d44013
zjx20 <zhoujianxiong2@gmail.com>
2025-09-21 14:58:33 +0800

    add deepseek-r1 to codebuddy provider

--- migrations/0005_add_code_buddy_provider.sql
+++ migrations/0005_add_code_buddy_provider.sql
@@ -2,5 +2,5 @@
 INSERT INTO "Provider" ("name", "throttleMode", "models") VALUES (
 	'CODE_BUDDY',
 	'BY_KEY',
-	'["claude-4.0", "gemini-2.5-flash", "gemini-2.5-pro", "gpt-5", "gpt-5-mini", "gpt-5-nano", "o4-mini", "deepseek-v3-1-lkeap", "deepseek-v3-0324-lkeap"]'
+	'["claude-4.0", "gemini-2.5-flash", "gemini-2.5-pro", "gpt-5", "gpt-5-mini", "gpt-5-nano", "o4-mini", "deepseek-v3-1-lkeap", "deepseek-v3-0324-lkeap", "deepseek-r1-0528-lkeap"]'
 );
